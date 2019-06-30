e2ee93d100c6641414928f30b09a66ad6f767e77
Xiang Xu <shawn@Xiangs-MacBook-Pro.local>
2019-06-30 19:25:28 -0400

    Edit config. TEST COMMIT

--- config/index.js
+++ config/index.js
@@ -1,19 +1,19 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
 
-  siteTitle: 'Jodie', // Navigation and Site Title
-  siteTitleAlt: 'Jodie - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteTitleShort: 'Jodie', // short_name for manifest
+  siteTitle: 'Shawn', // Navigation and Site Title
+  siteTitleAlt: 'Shawn - ANXXWORK Portfolio', // Alternative Site title for SEO
+  siteTitleShort: 'Shawn', // short_name for manifest
   siteHeadline: 'Come & Enjoy our excellent photos', // Headline for schema.org JSONLD
-  siteUrl: 'https://jodie.lekoarts.de', // Domain of your site. No trailing slash!
+  siteUrl: 'https://photos.mig21dc.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo.png', // Used for SEO and manifest
   siteDescription: 'Image-heavy photography portfolio with colorful accents & great typography',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  author: 'ANXXWORK', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@jodie', // Twitter Username
-  ogSiteName: 'jodie', // Facebook Site Name
+  userTwitter: '@Mig21PF', // Twitter Username
+  ogSiteName: 'xiang.xu.359', // Facebook Site Name
   ogLanguage: 'en_US', // og:language
   googleAnalyticsID: 'UA-XXXXXX-X',
 
